d101cab6bfd12c686193834c93557478551560d4
Akashgupta331999 <56925684+Akashgupta331999@users.noreply.github.com>
2022-02-12 21:54:28 +0530

    Update covid PortfolioProject.sql

--- covid PortfolioProject.sql
+++ covid PortfolioProject.sql
@@ -18,38 +18,37 @@ SELECT location,date,total_cases,total_deaths,(total_deaths/total_cases)*100 AS 
   FROM [dbo].[CovidDeath]
   where location = 'india'AND continent is not null
   order by 1,2 
-  -- looking of country with high infection rate
-    SELECT location,population,max(total_cases) as Highinfectioncount,max((total_cases/population)*100) AS casePercentage
-  FROM [dbo].[CovidDeath]
-  WHERE continent is not null
-  Group BY location, population
-  order by casePercentage DESC ;
 
-  -- looking for high amount of death with covid
-
-  SELECT location,max(CAST(total_deaths as INT))  AS TOTALDEATHCOUNT
-  FROM [dbo].[CovidDeath] 
-  WHERE continent is not null
- group by location
-  order by  TOTALDEATHCOUNT DESC;
-
-
-  -- SHOWING CONTINENT WITH HIGHEST DEATH COUNT PER POPULATION
-
-      SELECT continent,max(CAST(total_deaths as INT))  AS TOTALDEATHCOUNT
-  FROM [dbo].[CovidDeath] 
-  WHERE continent is not null
- group by continent
-  order by TOTALDEATHCOUNT DESC;
-
-  -- GLOBAL NUMBER
-
-  SELECT date,sum(new_cases)as total_case,sum(cast(new_deaths as int)) as total_death,
-  sum (cast(new_deaths as int))/sum(new_cases)*100 AS DeathPercentage
+  -- 1) GLOBAL NUMBER
+ 
+  SELECT sum(new_cases) as total_case,sum(cast(new_deaths as int)) as total_death,
+  sum(cast(new_deaths as int))/sum(new_cases)*100 AS DeathPercentage
   FROM [dbo].[CovidDeath]
   where  continent is not null
-  GROUP BY date
-  order by 1,2 
+  order by 1,2 ;
+  
+  -- 2) looking for high amount of death with covid
+
+  SELECT continent,SUM(CAST(new_deaths as INT))  AS TOTALDEATHCOUNT
+  FROM [dbo].[CovidDeath] 
+  WHERE continent  is not null
+  AND location not in ('WORLD','European Union' , 'International')
+ group by continent
+  order by  TOTALDEATHCOUNT DESC;
+  
+   -- 3) looking of country with high infection rate
+    SELECT location,population,max(total_cases) as Highinfectioncount,max((total_cases/population)*100) AS PercentPopulationInfected
+  FROM [dbo].[CovidDeath]
+ -- WHERE continent is not null
+  Group BY location, population
+  order by PercentPopulationInfected DESC ;
+  
+  -- 4) Percent Population Infected
+   SELECT location,date,population,max(total_cases) as Highinfectioncount,max((total_cases/population)*100) AS PercentPopulationInfected
+  FROM [dbo].[CovidDeath]
+ -- WHERE continent is not null
+  Group BY location, population,date
+  order by PercentPopulationInfected DESC ;
 
   -- total death
 
@@ -58,6 +57,7 @@ SELECT location,date,total_cases,total_deaths,(total_deaths/total_cases)*100 AS 
   FROM [dbo].[CovidDeath]
   where  continent is not null
   order by 1,2 ;
+  
   --looking at total population vs vaccination
   SELECT dea.continent , dea.location,dea.date,dea.population,vac.new_vaccinations,
   SUM(cast(vac.new_vaccinations as bigint)) OVER (partition by dea.location order by dea.location , dea.date) as RolingPeopleVaccinated
